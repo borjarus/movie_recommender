48ef1c7d1f91aa8711b6fd0ecf455ba34811858a
Miroslaw Puczynski <miroslaw@puczynski.org>
2023-09-15 15:18:27 +0200

    add getRandomTitles function

--- src/services/Recommender.php
+++ src/services/Recommender.php
@@ -4,8 +4,29 @@ namespace App\services;
 
 class Recommender
 {
-
-    public function getRandomTitles()
+    private array $movies;
+    public function __construct()
     {
+        $this->loadMovieList();
+    }
+
+    /**
+     * Get n random movie titles
+     * @param int $number number of random titles
+     * @return array
+     */
+    public function getRandomTitles(int $number = 3):array
+    {
+        $r = new \Random\Randomizer();
+        return array_slice($r->shuffleArray($this->movies),0,$count);
+    }
+
+    /**
+     * Load movie list to service
+     * @return void
+     */
+    private function loadMovieList()
+    {
+        $this->movies = (require(__DIR__.'/../data/movies.php') ?: []);
     }
 }
--- test/services/RecommenderTest.php
+++ test/services/RecommenderTest.php
@@ -10,6 +10,15 @@ class RecommenderTest extends TestCase
     public function testReturnsThreeRandomTitles()
     {
         $service = new Recommender();
-        $service->getRandomTitles();
+        $titles = $service->getRandomTitles();
+
+        // czy zwrócona wartość to tablica
+        $this->assertIsArray($titles);
+
+        // czy zwrócona tablica ma dokładnie 3 elementy
+        $this->assertCount(3, $titles);
+
+        // czy tytuły są unikalne
+        $this->assertTrue(array_unique($titles) === $titles);
     }
 }
